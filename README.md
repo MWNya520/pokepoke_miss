6186b358db50b3c2fab5c86ee47ef39d7a327a03
shengwang52005 <shengwang52005@163.com>
2024-08-14 00:13:26 +0800

    feat: 戳一戳回复

---
+++ README.md
@@ -0,0 +1,45 @@
+# pokepoke_miss
+
+## 介绍
+
+wmc的戳一戳回复插件，
+
+消息内容、概率都可以定制喔~
+
+## 功能
+
+- 对机器人执行戳一戳操作时，根据概率选择一个消息进行回复
+- 预设：
+
+  - `MISS` - 概率 10%
+  - `GOOD` - 概率 10%
+  - `GREAT` - 概率 30%
+  - `PERFECT` - 概率 40%
+  - `CRITICAL PERFECT` - 概率 10%
+
+## 安装
+
+### 手动安装
+
+1. 将 `pokepoke_miss.py` 文件放入你的 Nonebot 插件目录中
+
+2. 在 `bot.py`中写入以下代码：
+
+   ```python
+   nonebot.load_plugin("pokepoke_miss")
+
+## 自定义
+
+根据需求修改 `message_list` 中的消息内容和对应的概率，例如：
+
+```python
+message_list = [
+    ("喵", 0.2),
+    ("喵喵", 0.3),
+    ("喵喵喵", 0.5)
+]
+```
+
+## 许可
+
+MIT License
---
+++ __init__.py
@@ -0,0 +1,26 @@
+from nonebot import on_notice
+from nonebot.adapters.onebot.v11 import PokeNotifyEvent, Bot, Event
+
+import random
+
+# 戳一戳 Matcher
+poke = on_notice()
+
+# 消息列表
+message_list = [
+    ("MISS",0.2),
+    ("GOOD",0.2),
+    ("GREAT",0.5),
+    ("PERFECT",0.4),
+    ("CRITICAL PERFECT",0.2)
+]
+
+@poke.handle()
+async def handle_poke(bot: Bot, event: PokeNotifyEvent):
+    # 确认是戳机器人自己
+    if event.target_id == event.self_id:
+        # 分离消息和概率
+        messages, weights = zip(*message_list)
+        # 根据概率选一个消息回复
+        reply = random.choices(messages, weights=weights, k=1)[0]
+        await poke.send(event, reply)
