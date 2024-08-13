9c04779bddd7116d3fa9af35c5d40f8886a04953
shengwang52005 <shengwang52005@163.com>
2024-08-14 01:18:31 +0800

    fix: 确保插件被正确引入, release: 0.1.4gi

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from nonebot.plugin import PluginMetadata
+
+from .pokepoke_miss import *
+
+# 插件元数据
+__plugin_meta__ = PluginMetadata(
+    name="pokepoke_miss",
+    description="wmc的戳一戳回复插件，消息内容、概率都可以定制喔~",
+    usage="戳一戳回复，提供娱乐功能",
+    type="application",
+    extra={},
+)
